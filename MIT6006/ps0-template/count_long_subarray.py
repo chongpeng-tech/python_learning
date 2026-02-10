d4f74e4ab84c8bf272e4a3570dcbac2c3daa4874
chongpeng-tech <245660042+chongpeng-tech@users.noreply.github.com>
2026-02-11 00:48:17 +0800

    麻省理工算法导论作业1完成

--- MIT6006/ps0-template/count_long_subarray.py
+++ MIT6006/ps0-template/count_long_subarray.py
@@ -7,4 +7,32 @@ def count_long_subarray(A):
     ##################
     # YOUR CODE HERE #
     ##################
+    current_len = 1
+    max_len = 0
+    
+    for i in range(1, len(A)):
+        if A[i] > A[i - 1]:
+            current_len += 1
+        else:
+            if current_len > max_len:
+                max_len = current_len
+                current_len = 1
+                count = 1
+            elif current_len == max_len:
+                count += 1
+                current_len = 1
+            else:
+                current_len = 1
+        
+    if current_len > max_len:
+        
+        max_len = current_len
+        current_len = 1
+        count = 1
+    elif current_len == max_len:
+        count += 1
+        current_len = 1
+    else:
+        current_len = 1            
+    
     return count
